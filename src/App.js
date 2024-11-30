3bdb38ec1f335449415044a1f551844dc70c7eda
Đặng Thông Nghĩa <dangnghia2385@gmail.com>
2024-11-30 17:29:10 +0700

    up basename

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { CarProvider } from "./context/CarContext";
-import Home from "./components/Home"; // Trang chủ hiển thị danh sách xe
-import CarDetails from "./components/CarDetails"; // Chi tiết xe
-import RentCars from "./components/RentCars"; // Thuê xe
-import CarInventory from "./components/CarInventory"; // Quản lý xe
-import Navbar from "./components/Navbar"; // Thanh điều hướng
-import RentalHistory from "./components/RentalHistory"; // Lịch sử thuê xe
-import CarList from "./components/CarList"; // Hiển thị danh sách xe
-import cars from "./data/cars"; // Dữ liệu mẫu cho danh sách xe
-import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap cho giao diện
-import Footer from './components/Footer'; // Import Footer component
+import Home from "./components/Home";
+import CarDetails from "./components/CarDetails";
+import RentCars from "./components/RentCars";
+import CarInventory from "./components/CarInventory";
+import Navbar from "./components/Navbar";
+import RentalHistory from "./components/RentalHistory";
+import CarList from "./components/CarList";
+import cars from "./data/cars";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/Footer';
 
-const App = () => {
+function App() {
   return (
     <>
-      <CarProvider> 
-
-          <Navbar /> {/* Thanh điều hướng */}
+      <CarProvider>
+        <div>
+          <Navbar />
           <Footer />
           <Routes>
-            {/* Các route chính */}
             <Route path="/" element={<Home />} />
             <Route path="/rent" element={<RentCars />} />
             <Route path="/inventory" element={<CarInventory />} />
             <Route path="/rental-history" element={<RentalHistory />} />
-            { <Route path="/cars" element={<CarList cars={cars} />}   />  /*Sử dụng CarList tại một route riêng */}
-            <Route path="/car/:id" element={<CarDetails />} /> {/* Route chi tiết xe */}
+            <Route path="/cars" element={<CarList cars={cars} />} />
+            <Route path="/car/:id" element={<CarDetails />} />
           </Routes>
+        </div>
       </CarProvider>
-          </>
-
+    </>
   );
 }
 
